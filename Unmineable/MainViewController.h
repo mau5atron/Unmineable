5ff9356b56457eaa174fa85aca2b271784eb7654
mau5atron <gabrielbetancourt96@outlook.com>
2021-08-14 20:44:09 -0400

    Removed tests and changed background color. Added post build script

---
+++ Unmineable/MainViewController.h
@@ -0,0 +1,19 @@
+//
+//  MainViewController.h
+//  Unmineable
+//
+//  Created by Gabriel Betancourt on 8/14/21.
+//  Copyright © 2021 mau5atron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : NSViewController
+
+@property (weak) IBOutlet NSView *mainView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Unmineable/MainViewController.m
@@ -0,0 +1,25 @@
+//
+//  MainViewController.m
+//  Unmineable
+//
+//  Created by Gabriel Betancourt on 8/14/21.
+//  Copyright © 2021 mau5atron. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+	// poppins + fredoka one,cursive
+	
+	[self.mainView.layer setBackgroundColor:[[NSColor colorWithRed:44/255 green:44/255 blue:44/255 alpha:0.8] CGColor]];
+}
+
+@end
